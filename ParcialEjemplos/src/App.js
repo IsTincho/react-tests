564d5a0122b7413d36c309bf92cdce3b32690187
Martin Segnini <segninitincho@gmail.com>
2023-06-22 20:03:57 -0300

    NewExercise

--- ParcialEjemplos/src/App.js
+++ ParcialEjemplos/src/App.js
@@ -4,6 +4,7 @@ import Timer from "./components/Timer/Timer";
 import TodoList from "./components/TodoList/TodoList";
 import Toggle from "./components/Toggle/Toggle";
 import Calculator from "./components/00-EjercicioParaPracticar/Calculator/Calculator";
+import CurrencyConverter from "./components/01-EjercicioParaPracticar/CurrencyConverter/CurrencyConverter";
 function App() {
   return (
     <div className="App">
@@ -13,6 +14,7 @@ function App() {
       <Counter />
   <ColorPicker />*/}
       <Calculator />
+      <CurrencyConverter />
     </div>
   );
 }
