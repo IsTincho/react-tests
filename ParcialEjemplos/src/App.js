952774834281b643b8c87387c305dde4be99fcb4
Martin Segnini <segninitincho@gmail.com>
2023-06-22 19:57:24 -0300

    NewExampleAdded

--- ParcialEjemplos/src/App.js
+++ ParcialEjemplos/src/App.js
@@ -3,15 +3,16 @@ import Counter from "./components/Counter/Counter";
 import Timer from "./components/Timer/Timer";
 import TodoList from "./components/TodoList/TodoList";
 import Toggle from "./components/Toggle/Toggle";
-
+import Calculator from "./components/00-EjercicioParaPracticar/Calculator/Calculator";
 function App() {
   return (
     <div className="App">
-      <Toggle />
+      {/*<Toggle />
       <TodoList />
       <Timer />
       <Counter />
-      <ColorPicker />
+  <ColorPicker />*/}
+      <Calculator />
     </div>
   );
 }
